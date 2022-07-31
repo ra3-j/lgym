e5f35ef3a6f0f02f938bea6ea38a5c69068837f5
Ali Ahmad <ali.a@socienta.com>
2022-07-31 09:03:07 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  const now = 'test';
+  return (
+    <div>
+      <header>
+        <p>
+          Hello World ... {now}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
